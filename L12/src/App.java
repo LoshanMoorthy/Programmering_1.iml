01263e6d943e435f178a029ff6ba95fa03d9210e
losha <losh0001@gmail.com>
2023-03-08 11:59:54 +0100

    Programmering

--- L12/src/App.java
+++ L12/src/App.java
@@ -4,11 +4,6 @@ import javafx.scene.layout.Pane;
 public class App {
 
     public static void main(String[] args) {
-
-        Gui car = new Gui();
-
-        //car.drawCar(new Pane(), new Car());
-
         Application.launch(Gui.class);
     }
 }
